4fb915ff5a0503d2f9cec79312ea6f439be864e5
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-01-17 21:22:10 +0200

    Tax example work in progreess

---
+++ ConsoleApp3/ConsoleApp3.test/TaxTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApp3.test
+{
+    [TestClass]
+    public class TaxTests
+    {
+        [TestMethod]
+        public void TaxShouldBe5PercentWhenIncomeIsLess40000()
+        {
+            //Arange
+            //Act
+            //Assert
+        }
+    }
+}
