fc1886a21bda1e8c217a863db965ed61f6724fce
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-01-17 21:01:43 +0200

    l7 t2 work in progress

---
+++ ConsoleApp2/ConsoleApp2/BibliotekosKnyga.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    struct BibliotekosKnyga
+    {
+        int KnygosID;
+        public string KnygosPavadinimas;
+        public string SkaitytojoVardas;
+        public DateTime PaemimoData;
+
+        public BibliotekosKnyga(int bookid, string bname, string uname, DateTime pdate)
+        {
+            KnygosID = bookid;
+            KnygosPavadinimas = bname;
+            SkaitytojoVardas = uname;
+            PaemimoData = pdate;
+        }
+
+        public int PaskaiciuojaDienas()
+        {
+            DateTime today = DateTime.Now;
+            DateTime start = PaemimoData;
+            int dienos = (today - start).Days;
+            return dienos;
+        }
+
+    }
+
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BibliotekosKnyga book1 = new BibliotekosKnyga(1001, "Karibu Piratai", "Vardenis Pavardenis", new DateTime(2005, 12, 2));
+            BibliotekosKnyga book2 = new BibliotekosKnyga(1002, "Niezulys", "Jonas Jonaitis", new DateTime(2010, 10, 10));
+            BibliotekosKnyga book3 = new BibliotekosKnyga(1003, "Patranku Mesa", "Petras Petraitis", new DateTime(2019, 1, 30));
+            BibliotekosKnyga book4 = new BibliotekosKnyga(1004, "Ratai", "Antanas Antanaitis", new DateTime(2016, 2, 29));
+            BibliotekosKnyga book5 = new BibliotekosKnyga(1004, "Mohikanas", "Giedrius Giedraitis", new DateTime(2017, 2, 28));
+
+
+        }
+    }
+}
